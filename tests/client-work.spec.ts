5a743de9cb3b8ece749bbc152e03d192023abe25
JulsSmile <JulsHrenka@mail.ru>
2026-02-25 16:26:19 +0200

    Delete tests/client-work.spec.ts

--- tests/client-work.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('EPAM - Client Work link', () => {
-  test('placeholder', async ({ page }) => {
-    // TODO: replace with actual test
-  });
-});
