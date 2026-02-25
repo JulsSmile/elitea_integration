fcd80d139fd8ab6620abbe086d566704641667cc
JulsSmile <JulsHrenka@mail.ru>
2026-02-25 16:25:46 +0200

    Create tests/client-work.spec.ts

---
+++ tests/client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM - Client Work link', () => {
+  test('placeholder', async ({ page }) => {
+    // TODO: replace with actual test
+  });
+});
